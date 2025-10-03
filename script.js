1a20873af2de09685ccd7b7d44ff1a2e2fccba21
ktzazz <155502502+ktzazz@users.noreply.github.com>
2025-10-02 18:30:39 -0600

    mobile

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const mobileOpen = document.querySelector(".mobile__open");
   const mobileClose = document.querySelector(".mobile__close");
   const headerNav = document.querySelector(".header__nav");
+  const headerMenu = document.querySelector(".header__menu");
 
-  mobileBtn.addEventListener("click", function () {
-    if (mobileClose.classList.contains("inactive")) {
-      mobileOpen.classList.add("inactive");
-      mobileClose.classList.remove("inactive");
-      headerNav.style.display = "block";
-      headerNav.classList.add("open");
-    } else {
-      mobileOpen.classList.remove("inactive");
-      mobileClose.classList.add("inactive");
-      headerNav.style.display = "none";
-      headerNav.classList.remove("open");
-    }
-  });
+  const notDesktop = window.matchMedia("(max-width: 64em)").matches;
+
+  if (notDesktop) {
+    headerMenu.classList.add("mobile");
+    mobileBtn.addEventListener("click", function () {
+      if (mobileClose.classList.contains("inactive")) {
+        mobileOpen.classList.add("inactive");
+        mobileClose.classList.remove("inactive");
+        /* headerNav.style.display = "block"; */
+        headerMenu.classList.add("open");
+      } else {
+        mobileOpen.classList.remove("inactive");
+        mobileClose.classList.add("inactive");
+        /* headerNav.style.display = "none"; */
+        headerMenu.classList.remove("open");
+      }
+    });
+  }
 });
